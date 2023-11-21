6a6d7292e9e048e44d55a0e116c55b0d3778cb6e
hoang <hoang@wiserobot.com>
2023-11-21 15:18:33 +0700

    [Custom Project Module] update user_ids

--- custom_project/models/project.py
+++ custom_project/models/project.py
@@ -6,8 +6,5 @@ from odoo import fields, models, _
 class RegisterPayment(models.Model):
     _inherit = 'project.task'
 
-    user_ids = fields.Many2many(
-      'hr.employee',
-      string = "Assignees",
-    )
+    user_ids = fields.Many2many('hr.employee', relation='project_task_user_rel', column1='task_id', column2='user_id', string='Assignees', context={'active_test': False}, tracking=True)
 
