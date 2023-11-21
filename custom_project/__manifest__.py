22753e8ed2f281131ebfc32d793c1a616b61f60a
hoang <hoang@wiserobot.com>
2023-11-21 15:58:59 +0700

    [Custom Project Module] update name manifest

--- custom_project/__manifest__.py
+++ custom_project/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Custom Project',
+    'name': 'Custom Project Module',
     'version': '1.0',
     'description': 'Custom project module to modify the user_ids field from res.users to hr.employee',
     'depends': ['base', 'project'],
