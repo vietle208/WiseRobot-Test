f4c1f70bb842b8e126820908eb8a59af24fd55de
hoang <hoang@wiserobot.com>
2023-12-15 15:32:42 +0700

    [Planned Date] update pass

--- custom_project/models/project.py
+++ custom_project/models/project.py
@@ -8,7 +8,7 @@ class CustomProject(models.Model):
 
     @api.onchange('date_start', 'date')
     def _onchange_planned_date(self):
-        print("_onchange_planned_date")
+        pass
         # if not self.date and self.date_start:
         #     self.date_start = False
         # elif not self.date_start and self.date:
