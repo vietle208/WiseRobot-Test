05e895c140b61997c5324cd10923522b7592cffa
hoang <hoang@wiserobot.com>
2023-12-15 12:08:48 +0700

    [Planned Date] init

---
+++ custom_project/__mainfest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Custom Project Module',
+    'version': '1.0',
+    'description': 'Custom _onchange_planned_date',
+    'depends': ['base', 'project'],
+    'installable': True, 
+}
---
+++ custom_project/models/project.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from odoo import fields, models, _
+
+
+class CustomProject(models.Model):
+    _inherit = 'project.project'
+
+    @api.onchange('date_start', 'date')
+    def _onchange_planned_date(self):
+        print('Change planned date')
+        # if not self.date and self.date_start:
+        #     self.date_start = False
+        # elif not self.date_start and self.date:
+        #     self.date = False
+
